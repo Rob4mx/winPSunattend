b7ba048632a63a32f14d62f07c7595df0fd1503e
Rob4mx <41609089+Rob4mx@users.noreply.github.com>
2022-10-24 23:14:28 -0500

    Update master.ps1
    Agregados manejadores de errores.

--- master.ps1
+++ master.ps1
@@ -1,7 +1,11 @@
 echo '============================================================'
-echo 'Instalacion desatendida de drivers y programas de Windows.'
-echo 'Script v0.6 basado en PowerShell, por Rob.'
+echo '== Instalacion desatendida de drivers y programas de Windows.'
+echo '== Script v0.6 basado en PowerShell, por Rob.'
 echo '============================================================'
+echo ''
+echo ''
+echo ''
+echo ''
 
 # Variables de trabajo
 $RND = Get-Random -Maximum 9
@@ -12,22 +16,38 @@ $MOBO = $MOBO[2]
 
 # Codigo principal
 echo '************************ Copiando fondo de pantalla.'
-copy $DRIVE\FONDO_XT\BackgroundXtreme$RND.jpg $HOME\Pictures
+try {
+    copy $DRIVE\FONDO_XT\BackgroundXtreme$RND.jpg $HOME\Pictures -ErrorAction Stop
+    echo 'Copiado'
+} catch {
+    echo 'Archivo no encontrado. ¡Verifique los archivos de la carpeta FONDO_XT!'
+}
 
 echo '************************ Tarjeta madre:'
 echo $MOBO
+echo '************************'
 
 if ($CHIPSET -eq 'GenuineIntel') {
 # https://www.intel.com/content/www/us/en/download/19347/chipset-inf-utility.html?
     echo 'Detectado Chipset Intel. Instalando controladores...'
-    Start-Process -Wait $DRIVE\DRIVERS\Intel\Chipset\setup.exe -ArgumentList "-s -norestart"
-    echo 'Instalacion completada.'
+    try {
+        Start-Process -Wait $DRIVE\DRIVERS\Intel\Chipset\setup.exe -ArgumentList "-s -norestart" -ErrorAction Stop
+        echo 'Instalacion completada.'
+    } catch {
+        echo 'ERROR: Verificar archivos de instalacion en la carpeta DRIVERS\Intel\Chipset\setup.exe'
+        echo '¡Instalacion abortada!'
+    }
 }
 if ($CHIPSET -eq 'AuthenticAMD'){
 # https://www.amd.com/es/support/kb/release-notes/rn-ryzen-chipset-3-10-08-506
     echo 'Detectado Chipset AMD. Instalando controladores...'
-    Start-Process -Wait $DRIVE\DRIVERS\AMD\Chipset\setup.exe -ArgumentList "-install"
-    echo 'Instalacion completada.'
+    try {
+        Start-Process -Wait $DRIVE\DRIVERS\AMD\Chipset\setup.exe -ArgumentList "-install" -ErrorAction Stop
+        echo 'Instalacion completada.'
+    } catch {
+        echo 'ERROR: Verificar archivos de instalacion en la carpeta DRIVERS\AMD\Chipset\setup.exe'
+        echo '¡Instalacion abortada!'
+    }
 }
 
 echo '************************* Tarjeta de video:'
@@ -36,7 +56,11 @@ echo '************************* Tarjeta de video:'
 # https://www.intel.la/content/www/xl/es/download/19344/intel-graphics-windows-dch-drivers.html
 $GPU_Intel = pnputil /enum-devices /deviceid "PCI\VEN_8086"
 $GPU_Intel = $GPU_Intel -like "*PCI\*"
-$GPU_Intel = $GPU_Intel.Substring(37,17)
+try {
+    $GPU_Intel = $GPU_Intel.Substring(37,17)
+} catch {
+    echo 'No se encontraron graficos Intel en este equipo.'
+}
 
 $INF_Intel = Get-Content $DRIVE\DRIVERS\Intel\Video_6+\Graphics_Gen9_Gen11\iigd_dch.inf
 
@@ -51,7 +75,11 @@ for ($i=0; $i -le $GPU_Intel.Count-1; $i++) {
 # https://www.amd.com/en/support/kb/release-notes/rn-rad-win-22-10-2
 $GPU_AMD = pnputil /enum-devices /deviceid "PCI\VEN_1002"
 $GPU_AMD = $GPU_AMD -like "*PCI\*"
-$GPU_AMD = $GPU_AMD.Substring(37,17)
+try {
+    $GPU_AMD = $GPU_AMD.Substring(37,17)
+} catch {
+    echo 'No se encontraron graficos AMD en este equipo.'
+}
 
 $INF_AMD = Get-Content $DRIVE\DRIVERS\AMD\Radeon\Packages\Drivers\Display\WT6A_INF\U0384626.inf
 
@@ -65,7 +93,11 @@ for ($i=0; $i -le $GPU_AMD.Count-1; $i++) {
 # Para NVIDIA se usa el archivo "DRIVERS\NVIDIA\ListDevices.txt" generado con NVCleanstall.
 $GPU_NVIDIA = pnputil /enum-devices /deviceid "PCI\VEN_10DE"
 $GPU_NVIDIA = $GPU_NVIDIA -like "*PCI\*"
-$GPU_NVIDIA = $GPU_NVIDIA.Substring(37,17)
+try {
+    $GPU_NVIDIA = $GPU_NVIDIA.Substring(37,17)
+} catch {
+    echo 'No se encontraron graficos NVIDIA en este equipo.'
+}
 
 $INF_NVIDIA = Get-Content $DRIVE\DRIVERS\NVIDIA\ListDevices.txt
 
@@ -77,8 +109,14 @@ for ($i=0; $i -le $GPU_NVIDIA.Count-1; $i++) {
 }
 
 echo '********************************** Instalando Office...'
-Start-Process -Wait $DRIVE\OFFICE_2013\setup.exe
-echo 'Instalacion completada.'
+try {
+    Start-Process -Wait $DRIVE\OFFICE_2013\setup.exe -ErrorAction Stop
+    echo 'Instalacion completada.'
+} catch {
+    echo '¡Verificar archivos de instalacion de Office en la carpeta OFFICE_2013!'
+    echo '¡Instalacion abortada!'
+}
+
 
 echo '********************************** Pre-activando Windows...'
 # Agradecimiento al paquete de scripts del usuario "massgravel" de GitHub (https://github.com/massgravel/Microsoft-Activation-Scripts)
