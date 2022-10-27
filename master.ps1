d2d6dc8f5f6a1bf13f79887f1d15dfd413bee8a8
Rob4mx <41609089+Rob4mx@users.noreply.github.com>
2022-10-26 20:37:08 -0500

    Update master.ps1
    Agregado arte ASCII y mejorada presentación de información

--- master.ps1
+++ master.ps1
@@ -1,12 +1,27 @@
 Start-Transcript -path $HOME\Desktop\log.txt -append
-echo '============================================================'
-echo '== Instalacion desatendida de drivers y programas de Windows.'
-echo '== Script v0.6 basado en PowerShell, por Rob.'
-echo '============================================================'
-echo ''
-echo ''
-echo ''
-echo ''
+
+Write-Host @"
+
+             ,----------------,              ,---------,
+        ,-----------------------,          ,"        ,"|
+      ,"                      ,"|        ,"        ,"  |
+     +-----------------------+  |      ,"        ,"    |
+     |   -----------------.  |  |     +---------+      |
+     |  |                 |  |  |     | -==----'|      |               %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+     |  |  No te veo      |  |  |     |         |      |               % Instalacion desatendida de drivers y programas. %
+     |  |  :(             |  |  |/----|'---=    |      |               % Script v0.6 basado en PowerShell, de Rob.       %
+     |  |  <3             |  |  |   ,/|==== ooo |      ;               %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+     |  |                 |  |  |  // |(((( [33]|    ,"
+     |  '-----------------'  |," .;'| |((((     |  ,"
+     +-----------------------+  ;;  | |         |,"     -Kevin Lam-
+        /_)______________(_/  //'   | +---------+
+   ___________________________/___  `,
+  /  oooooooooooooooo  .o.  oooo /,   \,"-----------
+ / ==ooooooooooooooo==.o.  ooo= //   ,`\--{)B     ,"
+/_==__==========__==_ooo__ooo=_/'   /___________,"
+`-----------------------------'
+
+"@
 
 # Variables de trabajo
 $RND = Get-Random -Maximum 9
@@ -16,44 +31,44 @@ $MOBO = wmic baseboard get manufacturer,product
 $MOBO = $MOBO[2]
 
 # Codigo principal
-echo '************************ Copiando fondo de pantalla.'
+Write-Host '---------- Copiando fondo de pantalla ----------'
 try {
     copy $DRIVE\FONDO_XT\BackgroundXtreme$RND.jpg $HOME\Pictures -ErrorAction Stop
-    echo 'Copiado'
+    Write-Host 'Listo'
 } catch {
-    echo 'Archivo no encontrado. ¡Verifique los archivos de la carpeta FONDO_XT!'
+    Write-Host 'ERROR: Verifique los archivos de la carpeta FONDO_XT' -BackgroundColor 'Red'
 }
+Write-Host ''
 
-echo '************************ Tarjeta madre:'
-echo $MOBO
-echo '************************'
+Write-Host '---------- Tarjeta madre ----------'
+Write-Host $MOBO
 
 if ($CHIPSET -eq 'GenuineIntel') {
 # https://www.intel.com/content/www/us/en/download/19347/chipset-inf-utility.html?
-    echo 'Detectado Chipset Intel. Instalando controladores...'
+    Write-Host 'Detectado Chipset Intel. Instalando controladores...'
     try {
         Start-Process -Wait $DRIVE\DRIVERS\Intel\Chipset\setup.exe -ArgumentList "-s -norestart" -ErrorAction Stop
-        echo 'Instalacion completada.'
+        Write-Host 'Instalacion completada.'
     } catch {
-        echo 'ERROR: Verificar archivos de instalacion en la carpeta DRIVERS\Intel\Chipset\setup.exe'
-        echo '¡Instalacion abortada!'
+        Write-Host 'ERROR: Verificar archivos de instalacion en la carpeta DRIVERS\Intel\Chipset\setup.exe' -BackgroundColor 'Red'
+        Write-Host '¡Instalacion abortada!' -BackgroundColor 'Red'
     }
 }
 if ($CHIPSET -eq 'AuthenticAMD'){
 # https://www.amd.com/es/support/kb/release-notes/rn-ryzen-chipset-3-10-08-506
-    echo 'Detectado Chipset AMD. Instalando controladores...'
+    Write-Host 'Detectado Chipset AMD. Instalando controladores...'
     try {
         Start-Process -Wait $DRIVE\DRIVERS\AMD\Chipset\setup.exe -ArgumentList "-install" -ErrorAction Stop
-        echo 'Instalacion completada.'
+        Write-Host 'Instalacion completada.'
     } catch {
-        echo 'ERROR: Verificar archivos de instalacion en la carpeta DRIVERS\AMD\Chipset\setup.exe'
-        echo '¡Instalacion abortada!'
+        Write-Host 'ERROR: Verificar archivos de instalacion en la carpeta DRIVERS\AMD\Chipset\setup.exe' -BackgroundColor 'Red'
+        Write-Host '¡Instalacion abortada!' -BackgroundColor 'Red'
     }
 }
+Write-Host ''
 
-echo '************************* Tarjeta de video:'
-# Para Intel se usa el archivo "DRIVERS\Intel\Video_6+\Graphics_Gen9_Gen11\iigd_dch.inf" para cotejar con el
-# hardware instalado y decidir el driver a instalar.
+Write-Host '---------- Tarjeta de video ----------'
+# Para Intel se usa el archivo "DRIVERS\Intel\Video_6+\Graphics_Gen9_Gen11\iigd_dch.inf" para cotejar con el hardware instalado y decidir el driver a instalar.
 # https://www.intel.la/content/www/xl/es/download/19344/intel-graphics-windows-dch-drivers.html
 $INF_Intel = Get-Content $DRIVE\DRIVERS\Intel\Video_6+\Graphics_Gen9_Gen11\iigd_dch.inf
 $GPU_Intel = pnputil /enum-devices /class Display
@@ -61,12 +76,11 @@ $GPU_Intel = $GPU_Intel -like "*PCI\VEN_8086*"
 try {
     $GPU_Intel = $GPU_Intel.Substring(37,17)
 } catch {
-    echo 'No se encontraron graficos Intel en este equipo.'
+    Write-Host 'No se encontraron graficos Intel en este equipo.'
 }
-
 for ($i=0; $i -le $GPU_Intel.Count-1; $i++) {
     if ($INF_Intel -match $GPU_Intel[$i]) {
-        echo "Tarjeta de video Intel Graphics encontrada. Instalando controlador..."
+        Write-Host "Intel Graphics encontrado. Instalando controlador..."
         Start-Process -Wait $DRIVE\DRIVERS\Intel\Video_6+\Installer.exe -ArgumentList "-s"
     }
 }
@@ -79,12 +93,11 @@ $GPU_AMD = $GPU_AMD -like "*PCI\VEN_1002*"
 try {
     $GPU_AMD = $GPU_AMD.Substring(37,17)
 } catch {
-    echo 'No se encontraron graficos AMD en este equipo.'
+    Write-Host 'No se encontraron graficos AMD en este equipo.'
 }
-
 for ($i=0; $i -le $GPU_AMD.Count-1; $i++) {
     if ($INF_AMD -match $GPU_AMD[$i]) {
-        echo "Tarjeta de video AMD Radeon encontrada. Instalando controlador..."
+        Write-Host "AMD Radeon encontrado. Instalando controlador..."
         Start-Process -Wait $DRIVE\DRIVERS\AMD\Radeon\setup.exe -ArgumentList "-install"
     }
 }
@@ -96,57 +109,70 @@ $GPU_NVIDIA = $GPU_NVIDIA -like "*PCI\VEN_10DE*"
 try {
     $GPU_NVIDIA = $GPU_NVIDIA.Substring(37,17)
 } catch {
-    echo 'No se encontraron graficos NVIDIA en este equipo.'
+    Write-Host 'No se encontraron graficos NVIDIA en este equipo.'
 }
-
 for ($i=0; $i -le $GPU_NVIDIA.Count-1; $i++) {
     if ($INF_NVIDIA -match $GPU_NVIDIA[$i]) {
-        echo "Tarjeta de video NVIDIA GeForce encontrada. Instalando controlador..."
+        Write-Host "NVIDIA GeForce encontrado. Instalando controlador..."
         Start-Process -Wait $DRIVE\DRIVERS\NVIDIA\setup.exe -ArgumentList "-s"
     }
 }
+Write-Host ''
 
-echo '********************************** Instalando controladores de audio compatibles...'
+Write-Host '---------- Instalando controladores de audio compatibles... ----------'
 try {
     Start-Process -Wait $DRIVE\DRIVERS\Realtek\HD825E\Setup.exe -ArgumentList "/s" -ErrorAction Stop
-    echo 'Instalacion completada.'
+    Write-Host 'Listo'
 } catch {
-    echo '¡Verificar archivos de instalacion en la carpeta DRIVERS\Realtek\HD825E!'
+    Write-Host 'ERROR: Verificar archivos de instalacion en la carpeta DRIVERS\Realtek\HD825E' -BackgroundColor 'Red'
 }
+Write-Host ''
 
-echo '********************************** Instalando Office...'
+Write-Host '---------- Instalando Office... ----------'
 try {
     Start-Process -Wait $DRIVE\OFFICE_2013\setup.exe -ErrorAction Stop
-    echo 'Instalacion completada.'
+    Write-Host 'Listo'
 } catch {
-    echo '¡Verificar archivos de instalacion de Office en la carpeta OFFICE_2013!'
-    echo '¡Instalacion abortada!'
+    Write-Host 'ERROR: Verificar archivos de instalacion de Office en la carpeta OFFICE_2013' -BackgroundColor 'Red'
 }
+Write-Host ''
 
+Write-Host '---------- Pre-activando Windows... ----------'
+# Agradecimiento al paquete "MAS v1.6" del usuario "massgravel" de GitHub (https://github.com/massgravel/Microsoft-Activation-Scripts)
+try {
+    Start-Process -Wait $DRIVE\ACTIVADOR\Separate-Files-Version\HWID-KMS38_Activation\KMS38_Activation.cmd -ArgumentList "/a"
+    Start-Process -Wait $DRIVE\ACTIVADOR\Separate-Files-Version\HWID-KMS38_Activation\HWID_Activation.cmd -ArgumentList "/a"
+    Write-Host 'Listo'
+} catch {
+    Write-Host 'ERROR: Verificar archivos de instalacion de la carpeta ACTIVADOR' -BackgroundColor 'Red'
+}
+Write-Host ''
 
-echo '********************************** Pre-activando Windows...'
-# Agradecimiento al paquete de scripts del usuario "massgravel" de GitHub (https://github.com/massgravel/Microsoft-Activation-Scripts)
-Start-Process -Wait $DRIVE\ACTIVADOR\Separate-Files-Version\HWID-KMS38_Activation\KMS38_Activation.cmd -ArgumentList "/a"
-Start-Process -Wait $DRIVE\ACTIVADOR\Separate-Files-Version\HWID-KMS38_Activation\HWID_Activation.cmd -ArgumentList "/a"
+Write-Host '---------- Pre-activando Office... ----------'
+# Agradecimiento al paquete "MAS v1.6" del usuario "massgravel" de GitHub (https://github.com/massgravel/Microsoft-Activation-Scripts)
+try {
+    Start-Process -Wait $DRIVE\ACTIVADOR\Separate-Files-Version\Online_KMS_Activation\Activate.cmd -ArgumentList "/a"
+    Write-Host 'Listo'
+} catch {
+    Write-Host 'ERROR: Verificar archivos de instalacion de la carpeta ACTIVADOR' -BackgroundColor 'Red'
+}
+Write-Host ''
 
-echo '********************************** Pre-activando Office...'
-# Agradecimiento al paquete MAS v1.6 del usuario "massgravel" de GitHub (https://github.com/massgravel/Microsoft-Activation-Scripts)
-Start-Process -Wait $DRIVE\ACTIVADOR\Separate-Files-Version\Online_KMS_Activation\Activate.cmd -ArgumentList "/a"
-Start-Process taskschd
-
-echo '********************************** Estableciendo fondo de pantalla...'
+Write-Host '---------- Estableciendo fondo de pantalla... ----------'
 Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name WallPaper -value $HOME\Pictures\BackgroundXtreme$RND.jpg
 for ($i=0; $i -le 10; $i++) {
     rundll32.exe user32.dll,UpdatePerUserSystemParameters ,1 ,True
 }
-echo 'Hecho.'
-echo '********************************** Ejecutando CrystalDiskInfo...'
+Write-Host 'Listo'
+Write-Host ''
+
+Write-Host '---------- Ejecutando CrystalDiskInfo... ----------'
 try {
     Start-Process -Wait $DRIVE\HERRAMIENTAS\CrystalDiskInfo\DiskInfo64.exe -ArgumentList "/CopyExit"
     Move-Item $DRIVE\HERRAMIENTAS\CrystalDiskInfo\DiskInfo.txt $HOME\Desktop
-    echo 'Creado archivo de reporte en el escritorio.'
+    Write-Host 'Creado archivo de reporte en el escritorio.'
 } catch {
-    echo '¡Verificar archivos de instalacion de CystalDiskInfo en la carpeta HERRAMIENTAS\CrystalDiskInfo!'
+    Write-Host 'ERROR: Verificar archivos de instalacion de CystalDiskInfo en la carpeta HERRAMIENTAS\CrystalDiskInfo' -BackgroundColor 'Red'
 }
 
 Stop-Transcript
