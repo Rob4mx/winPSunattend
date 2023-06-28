639c1ddb7f53d59d7b9c44f489ce478b9bf34d09
Rob4mx <41609089+Rob4mx@users.noreply.github.com>
2023-06-27 21:54:52 -0600

    Update master.cmd

--- master.cmd
+++ master.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
 cd /d "%~dp0" && (if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs") && fsutil dirty query %systemdrive% 1>nul 2>nul || (cmd /u /c echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0""", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 SET ruta=%~dp0
-SET rutaPowerShell=%ruta%master065.ps1
+SET rutaPowerShell=%ruta%master0666.ps1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%rutaPowerShell%'";
